d5ebfa9d060bab1151866700cf2bac8e11cccb40
lok <hasuteam@mail.com>
2021-05-04 16:09:02 +0300

    добавил функционал удаления постов и добавления новых

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 import AppHeader from '../app-header';
@@ -14,33 +14,76 @@ const AppBlock = styled.div`
     margin: 0 auto;
     max-width: 800px;
 `;
-
+//  наследование стилей
 // const StyledAppBlock = styled(AppBlock)`
 //     background-color:grey;
 // `;
 
 
-const App = () => {
+export default class App extends Component  {
 
-    const data = [
-        {label:"Going to learn React", important: true, id: 'wee'},
-        {label:"That is good", important: false, id: 'qwe'},
-        {label:"I nead a break...", important: false, id: 'eqwdd'}
-    ];
+    constructor(props){
+        super(props);
+        this.state = {
+            data : [
+                {label:"Going to learn React", important: true, id: 1},
+                {label:"That is good", important: false, id: 2},
+                {label:"I nead a break...", important: false, id: 3}
+            ]
+        };
+        this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
 
-    return (
-        <AppBlock>
-            <AppHeader/>
-            <div className='search-panel d-flex'>
-                <SearchPanel/>
-                <PostStatusPanel/>
-            </div>
-            <PostList posts={data}/>
-            <PostAddForm/>
-        </AppBlock>
-    )
+        this.maxId = 4;
+    }
+    deleteItem(id){
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+                //не изменяя стейт создаем новый массив
+            const before = data.slice(0, index);  
+            const after = data.slice(index + 1);
+
+            const newArr =[...before, ...after];
+            return{
+                data: newArr
+            }
+
+        });
+    }
+    addItem(body){
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return{
+                data:newArr
+            }
+        });
+    }
+
+    render(){
+        return (
+            <AppBlock>
+                <AppHeader/>
+                <div className='search-panel d-flex'>
+                    <SearchPanel/>
+                    <PostStatusPanel/>
+                </div>
+                <PostList posts={this.state.data}
+                onDelete={this.deleteItem}/>
+                <PostAddForm
+                onAdd={this.addItem}/>
+            </AppBlock>
+        )
+    }
+    
+
+    
 }
 
-export default App; 
+ 
 
 
--- my-app/src/components/post-status-filter/post-status-filter.js
+++ my-app/src/components/post-status-filter/post-status-filter.js
@@ -6,7 +6,7 @@ const PostStatusPanel = () => {
     return(
         <div className="btn-group">
             <Button outline color='info'>Все</Button>
-            <button type="button" className="btn btn-info">Все</button>
+            
             <button type="button" className="btn btn-outline-secondary">Понравилось</button>
         </div>
     )
